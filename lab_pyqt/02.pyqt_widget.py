0738e5a420ef1c14959a1f45c43f7596e78e3fd9
picachu3333 <yjekim0315@naver.com>
2021-07-10 16:13:16 +0900

    .

---
+++ lab_pyqt/02.pyqt_widget.py
@@ -0,0 +1,43 @@
+#02.pyqt_widget
+#PtQt 위젯
+import sys
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+import numpy as np
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        #창 크기 고정
+        self.setFixedSize(1600,900)
+        #창 제목 설정
+        self.setWindowTitle('GA Mairo')
+
+        #버튼
+        button = QPushButton(self)
+        button.setText('버튼')
+        button.setGeometry(500, 5-00, 500, 500)
+
+        #텍스트
+        label_text = QLabel(self)
+        label_text.setText('가나다')
+        label_text.setGeometry(200, 150, 300, 400)
+
+        #이미지
+        label_image = QLabel(self)
+
+        image = np.array([[[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0]]])
+        qimage = QImage(image,image.shape[1], image.shape[0],QImage.Format_RGB888)
+        pixmap = QPixmap(qimage)
+        pixmap = pixmap.scaled(1400,1200,Qt.IgnoreAspectRatio)
+
+        label_image.setPixmap(pixmap)
+        label_image.setGeometry(0,0,1400,200)
+
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyApp()
+    sys.exit(app.exec_())
